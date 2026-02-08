0fec2366bda12b83293f5c2ca58cdd8bda2f49fd
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2026-02-08 17:22:17 +0000

    Update entrypoint to strip quotes from package envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 set -e
 
+strip_quotes() {
+  local string="$1"
+  string=${string#\"}
+  string=${string%\"}
+  string=${string#\'}
+  string=${string%\'}
+  echo "$string"
+}
+
+fix_perms() {
+  local path="$1"
+  local user="$(id -u)"
+  local group="$(id -g)"
+
+  sudo find "$path" -mindepth 1 \
+    \( ! -user "$user" -o ! -group "$group" \) \
+    -exec chown "$user:$group" -- {} +
+}
+
 if [ -n "$APT_PACKAGES" ]; then
   echo "Installing apt packages"
   sudo apt-get update
-  sudo apt-get install -y $APT_PACKAGES
+  sudo apt-get install -y $(strip_quotes "$APT_PACKAGES")
   sudo rm -rf /var/lib/apt/lists/*
   echo
 fi
 
 if [ -n "$BREW_PACKAGES" ]; then
   echo "Installing brew packages"
-  brew install $BREW_PACKAGES
+  brew install $(strip_quotes "$BREW_PACKAGES")
   echo
 fi
 
 if [ -n "$BUN_PACKAGES" ]; then
   echo "Installing bun packages"
-  bun install -g $BUN_PACKAGES
+  bun install -g $(strip_quotes "$BUN_PACKAGES")
   echo
 fi
-
-fix_perms() {
-  local path="$1"
-  local user="$(id -u)"
-  local group="$(id -g)"
-  
-  if [ -d "$path" ]; then
-    sudo find "$path" -mindepth 1 \
-      \( ! -user "$user" -o ! -group "$group" \) \
-      -exec chown "$user:$group" -- {} +
-  fi
-}
 
 echo "Fixing permissions"
 fix_perms "$HOME"/.config
