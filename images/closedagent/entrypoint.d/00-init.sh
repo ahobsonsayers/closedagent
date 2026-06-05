e1214d6c82e3f216a77468a519ca817f0b6f10d0
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2026-06-05 19:45:13 +0000

    Fix fast chown

--- images/closedagent/entrypoint.d/00-init.sh
+++ images/closedagent/entrypoint.d/00-init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "export PATH="$PATH"" | tee -a /etc/profile > /dev/null
+echo "export PATH=\"$PATH\"" | tee -a /etc/profile > /dev/null
 
 echo "Fixing ownership"
 
 fast_chown() {
-  find "$1" ( ! -uid 1000 -o ! -gid 1000 ) -exec chown 1000:1000 {} +
+  find "$1" \( ! -uid 1000 -o ! -gid 1000 \) -exec chown 1000:1000 {} +
 }
 
 fast_chown "$HOME/.cache" || true
