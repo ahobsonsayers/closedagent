9135ff6d81ed85f864b40b1878712ce8139a1df9
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2026-06-05 22:33:16 +0000

    Use fd instead of find

--- images/closedagent/entrypoint.d/00-init.sh
+++ images/closedagent/entrypoint.d/00-init.sh
@@ -6,7 +6,8 @@ echo "export PATH=\"$PATH\"" | tee -a /etc/profile > /dev/null
 echo "Fixing ownership"
 
 fast_chown() {
-  find "$1" \( ! -uid 1000 -o ! -gid 1000 \) -exec chown 1000:1000 {} +
+  chown 1000:1000 "$1"
+  fdfind --hidden --no-ignore --owner '!1000:' . "$1" -X chown 1000:1000
 }
 
 fast_chown "$HOME/.cache" || true
